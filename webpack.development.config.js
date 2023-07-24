6fd2d53ce23e92bd3ceda66946b16c7b60c4c387
santoshpmca <64215696+santoshpmca@users.noreply.github.com>
2023-07-24 18:47:56 +0530

    separate configs

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,17 +1,15 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.[contenthash].js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, './dist'),
     publicPath: '',
   },
-  mode: 'none',
+  mode: 'development',
   module: {
     rules: [
       {
@@ -20,11 +18,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.js$/,
@@ -44,10 +42,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new TerserPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css',
-    }),
     new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       title: 'Hello World',
