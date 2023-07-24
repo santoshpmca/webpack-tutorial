7ecbd696785e88c709ec56cdce6bafeaace0942a
santoshpmca <64215696+santoshpmca@users.noreply.github.com>
2023-07-24 13:28:06 +0530

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist'),
+    publicPath: 'dist/',
   },
   mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
